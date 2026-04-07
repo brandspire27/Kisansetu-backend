4c8dd2e9d820deaf18515b2d0cc6e5bf716a8ed9
Tanishk Tyagi <tanishkt17@gmail.com>
2026-04-07 12:56:58 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,16 @@ const { register, login } = require("../controllers/authController");
 
 router.post("/register", register);
 router.post("/login", login);
+// Get all farmers
+router.get("/farmers", async (req, res) => {
+    const farmers = await User.find({ role: "farmer" });
+    res.json(farmers);
+});
+
+// Get all consumers
+router.get("/consumers", async (req, res) => {
+    const consumers = await User.find({ role: "consumer" });
+    res.json(consumers);
+});
 
 module.exports = router;
